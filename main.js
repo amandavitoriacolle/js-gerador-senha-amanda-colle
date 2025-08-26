326368d43b33f5e0cc362bc013b0286c1a5d5901
amandavitoriacolle <145784896+amandavitoriacolle@users.noreply.github.com>
2025-08-26 08:15:45 -0300

    vvvvv

--- main.js
+++ main.js
@@ -2,13 +2,23 @@ const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
-const botao = document.querySelectorAll('.parametro-senha__botao');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){
-    if(tamanhoSenha > 1){
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
         tamanhoSenha = tamanhoSenha - 1;
     }
+    numeroSenha.textContent = tamanhoSenha;
 }
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        tamanhoSenha = tamanhoSenha + 1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+
+}
+
