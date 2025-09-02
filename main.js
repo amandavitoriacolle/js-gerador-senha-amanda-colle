237080e17c51c0545ec8817eae6a8061b9eb5831
amandavitoriacolle <145784896+amandavitoriacolle@users.noreply.github.com>
2025-09-02 08:29:04 -0300

    hh

--- main.js
+++ main.js
@@ -29,25 +29,7 @@ const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
 const numeros = '0123456789';
 const simbolos = '#%$8&@?!';
- 
 function geraSenha() {
-    let alfabeto = '';
-    if (checkbox[0].checked) {
-        alfabeto = alfabeto + letrasMaiusculas;
-    }
-
-    if (checkbox[1].checked) {
-        alfabeto = alfabeto + letrasMinusculas;
-    }
-
-    if (checkbox[2].checked) {
-        alfabeto = alfabeto + numeros;
-    }
-
-    if (checkbox[3].checked) {
-        alfabeto = alfabeto + simbolos;
-    }
-
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random() * letrasMaiusculas.length;
@@ -55,4 +37,6 @@ function geraSenha() {
         senha = senha + letrasMaiusculas[numeroAleatorio];
     }
     campoSenha.value = senha;
+    classificaSenha();
 }
+
